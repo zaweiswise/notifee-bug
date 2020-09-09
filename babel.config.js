4dd14d5d71e336cf9f7995be9dcb3c629106062d
Zeeshan Abbasi <za@weiswise.com>
2020-09-09 11:52:27 +0500

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    "module:metro-react-native-babel-preset",
+    "@babel/preset-flow"
+  ],
+  plugins: [
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        "legacy": true
+      }
+    ]
+  ],
+  env: {
+    "production": {
+      "plugins": [
+        "transform-remove-console"
+      ]
+    }
+  }
+}
